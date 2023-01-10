c248be4d70f0e0e21bcb4a44effc98e6ffa842fb
jaqwe2301 <jaqwe2301@naver.com>
2023-01-11 00:51:07 +0900

    7강까지 함

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 4000
+// const bodyParser = require('body-parser')
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+//위 형식의 파일을 분석해줄 수 있는 코드
+app.use(express.urlencoded({extended: true}));
+
+//application/json
+//위 형식의 파일을 분석해줄 수 있는 코드
+app.use(express.json());
 
 const moogoose = require('mongoose')
-moogoose.connect('mongodb+srv://minhyeok:zlvj4381@nodejs-practice.frualie.mongodb.net/?retryWrites=true&w=majority', {
+moogoose.connect('mongodb+srv://minhyeok:zlvj4381@nodejs-practice.4xfzaz3.mongodb.net/?retryWrites=true&w=majority', {
 }).then(() => console.log('몽고DB 연결됨'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('워메, 만나서 반갑데이')
+})
+
+app.post('/register', (req, res) => {
+
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    // user에서 받은 데이터를 저장하는 코드, err이면 success : false
+    user.save((err, userInfo) => {
+      if(err) return res.json({ success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,36 +1,79 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-    name: {
+// mongoose.set('strictQuery', true);
+
+// const userSchema = mongoose.Schema({
+//     name: {
+//         type: String,
+//         maxlength: 50
+//     },
+//     email: {
+//         type: String,
+//         trim: true, // 띄어 쓰기를 없애는 역할 ex. asdf ghj@naver.com
+//         unique: 1 // 똑같은 이메일을 쓰지 못하게
+//     },
+//     password: {
+//         type: String,
+//         minlength: 5
+//     },
+//     lastname: {
+//         type: String,
+//         maxlength: 50 
+//     },
+//     role: { // 일반 유저(0), 관리자(1) 분리 
+//         type: Number,
+//         default: 0
+//     },
+//     image: String,
+//     token: { // 유효성 관리
+//         type: String
+//     },
+//     tokenExp: { // 토큰을 사용할 수 있는 기간 (유효기간)
+//         type: Number
+//     }
+// })
+
+// const User = mongoose.model('User', userSchema)
+
+// module.exports = { User }
+
+const mongoose=require('mongoose');
+//User 도메인 테이블을 생성하기 위한 스키마
+const userSchema=mongoose.Schema({
+    name:{
         type: String,
-        maxlength: 50
+        maxlength: 50,
     },
-    email: {
+    email:{
         type: String,
-        trim: true, // 띄어 쓰기를 없애는 역할 ex. asdf ghj@naver.com
-        unique: 1 // 똑같은 이메일을 쓰지 못하게
+        // space bar를 없애준다.
+        trim: true,
+        // 중복을 허용하지 않는다.
+        unique: 1
     },
     password: {
         type: String,
-        minlength: 5
+        minlength: 5,
     },
-    lastname: {
-        type: String,
-        maxlength: 50 
+    lastname:{
+        type:String,
+        maxlength:50,
     },
-    role: { // 일반 유저(0), 관리자(1) 분리 
-        type: Number,
-        default: 0
+    role:{
+        type:Number,
+        // 기본값
+        default: 0,
     },
     image: String,
-    token: { // 유효성 관리
-        type: String
+    token:{
+        type:String,
     },
-    tokenExp: { // 토큰을 사용할 수 있는 기간 (유효기간)
-        type: Number
+    tokenExp:{
+        type: Number,
     }
-})
+});
+// User를 model화 해준다.
+const User=mongoose.model('User', userSchema);
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = { User }
+// 다른 파일에서도 이 모델을 쓸 수 있도록
+module.exports={ User }
